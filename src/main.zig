8ae9d63446674c1d6d73a558f14666a7cdd2f11b
Kenneth Laskoski <kennethlaskoski@gmail.com>
2024-03-22 10:27:48 -0300

    Added initial code

---
+++ src/main.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const testing = std.testing;
+
+const Rank = u3;
+const File = u3;
+
+export fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+test "basic add functionality" {
+    try testing.expect(add(3, 7) == 10);
+}
